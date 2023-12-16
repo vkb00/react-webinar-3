05687f4eed3affd5628d1ecbf678459a6f8cde43
vkb7662 <vkb7662@gmail.com>
2023-12-16 12:16:25 +0300

    fixed server error zone

--- src/components/login-form/index.js
+++ src/components/login-form/index.js
@@ -1,24 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from "react-hook-form";
 import { useNavigate } from 'react-router-dom';
 import "./style.css"
 export const LoginForm = (props) => {
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const [error, setError] = useState('');
     const navigate = useNavigate();
     const onSubmit = async (data) => {
         console.log(data);
-        const response = await fetch('/api/v1/users/sign', {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            headers: {
-                "Content-Type": "application/json",
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: JSON.stringify(data),
-        });
-        const json = await response.json();
-        console.log(json.result.token);
-        localStorage.setItem('token', json.result.token);
-        navigate("/");
+        try {
+            const response = await fetch('/api/v1/users/sign', {
+                method: "POST", // *GET, POST, PUT, DELETE, etc.
+                headers: {
+                    "Content-Type": "application/json",
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: JSON.stringify(data),
+            });
+            const json = await response.json();
+            setError(json?.error?.message);
+            console.log(json);
+            localStorage.setItem('token', json.result.token);
+            navigate("/");
+        }
+        catch (e) {
+            console.log(error)
+        }
+
     }
     return (
         <form className='form' onSubmit={handleSubmit(onSubmit)}>
@@ -26,6 +34,7 @@ export const LoginForm = (props) => {
             <input {...register('login')} />
             <label>Пароль</label>
             <input {...register('password')} />
+            {error && <p className='error'>{error}</p>}
             <button type='submit'>Войти</button>
         </form>
     )
